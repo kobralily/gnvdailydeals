8947e435eb7bb7cfd8ee128af31766c3b6dd80e2
kobralily <87397507+kobralily@users.noreply.github.com>
2023-02-04 03:28:28 -0500

    corrected database file

--- database/deals.sql
+++ database/deals.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.9.7
+-- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
--- Host: localhost:3306
--- Generation Time: Feb 04, 2023 at 08:07 AM
--- Server version: 5.7.41
--- PHP Version: 7.4.33
+-- Host: localhost
+-- Generation Time: Feb 04, 2023 at 09:27 AM
+-- Server version: 10.4.27-MariaDB
+-- PHP Version: 8.2.0
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -19,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `oliviagi_gnvdailydeals`
+-- Database: `gnvdailydeals`
 --
 
 -- --------------------------------------------------------
@@ -27,41 +26,8 @@ SET time_zone = "+00:00";
 --
 -- Table structure for table `deals`
 --
-
-CREATE TABLE `deals` (
-  `id` int(11) NOT NULL,
-  `date` varchar(30) NOT NULL,
-  `location` varchar(30) NOT NULL,
-  `deal` varchar(140) NOT NULL,
-  `days` varchar(15) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `deals`
---
-
-INSERT INTO `deals` (`id`, `date`, `location`, `deal`, `days`) VALUES
-(1, 'test1', 'test2', 'test3', 'test4');
-
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `deals`
---
-ALTER TABLE `deals`
-  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `deals`
---
-ALTER TABLE `deals`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+-- Error reading structure for table gnvdailydeals.deals: #1142 - SHOW command denied to user &#039;&#039;@&#039;localhost&#039; for table `gnvdailydeals`.`deals`
+-- Error reading data for table gnvdailydeals.deals: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `gnvdailydeals`.`deals`&#039; at line 1
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
